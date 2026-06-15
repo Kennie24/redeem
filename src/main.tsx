5a0c9633226a35a5b685d289aeae2ecc04043012
Kennie24 <Kennie24@users.noreply.github.com>
2026-06-15 13:30:37 +0300

    Fix static routing for scan page

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
 import { StagewiseDevToolbar } from "./components/StagewiseDevToolbar.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
       <StagewiseDevToolbar />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
